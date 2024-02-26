a7d6d87fc2aba43fa21f3a505dcc0d2c5914df58
Moksh Gupta <115498284+mokshgupta03@users.noreply.github.com>
2024-02-26 22:44:34 +0530

    Create rect.java

---
+++ rect.java
@@ -0,0 +1,21 @@
+public class rectangle {
+    double a,b;
+     rectangle(double length, double width)
+    {
+        a=length;
+        b=width;
+    }
+
+    String check()
+    {
+        if(a==b)
+        {
+           return "Square";
+        }
+        else 
+        {
+           return "Not square";
+        }
+    }
+     
+}
